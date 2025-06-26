5b0f7ce5460031940b26fae1476b94daf1b1bd1c
aratislamsafol <aratislamsafol@gmail.com>
2025-06-26 12:40:36 +0600

    json response

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const express = require("express");
 app = express();
 
 app.get("/", (req, res)=> res.send("Hello Express"))
-app.post("/about", (req, res)=> res.send("About Express"))
 app.delete("/contact", (req, res)=> res.send("Contact Express"))
 app.put("/terms", (req, res)=> {
     res.status(201).end("Terms 201 Response")
+})
+app.post("/about", (req, res)=> {
+    let myJsonArr = [
+        {
+            name:'arat',
+            city: 'Jashore',
+            age: 26
+        },
+        {
+            name:'arat2',
+            city: 'Jashore',
+            age: 26
+        },
+        {
+            name:'arat3',
+            city: 'Jashore',
+            age: 26
+        },
+    ]
+
+    res.json(myJsonArr);
 })
 
 app.listen(8000, ()=>console.log("successfully listen"));
