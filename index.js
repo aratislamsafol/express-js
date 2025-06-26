f166202a8fe53ae84d8f933e72ec2c6a11376dc9
aratislamsafol <aratislamsafol@gmail.com>
2025-06-26 12:33:10 +0600

    simple status change, response string

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require("express");
+const express = require("express");
 
 app = express();
 
-app.get("/", function(req, res) {
-    res.send("express start");
+app.get("/", (req, res)=> res.send("Hello Express"))
+app.post("/about", (req, res)=> res.send("About Express"))
+app.delete("/contact", (req, res)=> res.send("Contact Express"))
+app.put("/terms", (req, res)=> {
+    res.status(201).end("Terms 201 Response")
 })
 
-app.listen(8000, function(){console.log("server run success")});
+app.listen(8000, ()=>console.log("successfully listen"));
