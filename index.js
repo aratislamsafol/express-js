3723e917213a5b3c0efd601d777984e043f8c5b2
aratislamsafol <aratislamsafol@gmail.com>
2025-06-27 15:42:58 +0600

    File Upload with multer

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require("express");
-const multers = require("multer");
-var multer =multers();
+const multer = require("multer");
 
 app = express();
-app.use(multer.array());
+
 app.use(express.static('public'))
 
+// parse json body
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 // Json Application request
 app.post("/", (req, res)=>{
     res.send(JSON.stringify(req.body));
+})
+// file upload
+
+var storage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, './upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+var upload = multer({storage:storage }).single('myfile');
+
+app.post("/upload", (req, res)=>{
+    upload(req, res, (err)=> {
+        if(err) {
+            return res.end("Error Uploading File")
+        }else {
+            res.end("file Uploaded")
+        }
+    })
 })
 
 app.listen(8000, ()=>console.log("successfully Hit the Server"));
