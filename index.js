71f40bf9040f5b05dc05a321c42117044920b9d4
aratislamsafol <aratislamsafol@gmail.com>
2025-06-27 11:39:04 +0600

    Json request

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 
 app = express();
+app.use(bodyParser.json());
 
-// post request URL Query
+// Json Application request
 app.post("/", (req, res)=>{
-    const firstName = req.query.firstName;
-    const lastName = req.query.lastName;
-    // post req ex - 
-    res.send(firstName +" " + lastName);
+    let jsonData = req.body;
+    let name = req.body.name;
+    let city = req.body['city'];
+    let stringify = JSON.stringify(jsonData)
+    res.send(stringify + " " + name +" " + city);
 })
 
-app.listen(8000, ()=>console.log("successfully listen"));
+app.listen(8000, ()=>console.log("successfully Hit the Server"));
